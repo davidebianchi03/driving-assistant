5def2041acde6eb14724ec6c444e23dcda2cda96
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2022-02-16 17:05:36 +0100

    Ottengo la posizione aggiornata su un thread separato

--- pyGPS/main.py
+++ pyGPS/main.py
@@ -1,11 +1,15 @@
+import time
+import sys
 from pygps import *
 
 gps = pygps(serialport_name='COM10')
+gps.Start()
 
 while True:
-    result = gps.GetPosition()
-    if result['valid']:
+    result = gps.GetLastKnownPosition()
+    if (not result == None) and result['valid']:
         print("Coordinates", result['latitude'], ',',result['longitude'])
         print("Time:", result['time'], " Date:", result['date'])
         print("Speed:", result['speed']*3.6)
         print()
+        time.sleep(1)
