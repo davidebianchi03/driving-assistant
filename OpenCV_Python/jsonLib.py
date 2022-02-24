0a5df31bad664a4da9292419259d93a6dbf22941
Emanuele <82151522+LelePaci@users.noreply.github.com>
2022-02-24 13:47:22 +0100

    trasformazione in json delle linee rilevate

---
+++ OpenCV_Python/jsonLib.py
@@ -0,0 +1,12 @@
+import json
+
+class myJSON:
+    def lineToJSON(x1, y1, x2, y2):
+        line = dict()
+        line["x1"]= int(x1)
+        line["y1"]= int(y1)
+        line["x2"]= int(x2)
+        line["y2"]= int(y1)
+        
+        return json.dumps(line)
+  
--- OpenCV_Python/main.py
+++ OpenCV_Python/main.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np 
 #import matplotlib.pyplot as plt
 
+from jsonLib import myJSON
+  
 def cannyImage(image):
     #trasforma l'immagine originale in una nuova immagine con meno dettagli da analizzare
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   
@@ -12,12 +14,10 @@ def cannyImage(image):
     return edges
 
 def regionOfInterest(image):
-    #ritaglia un triangolo nella parte inferiore dell'immagine
+    #ritaglia una zona di interessa nella parte inferiore dell'immagine
     #per identificare solamente la carreggiata
     height = image.shape[0]
-    width = image.shape[1]
-    #triangolo definito nella parte inferiore dell'immagine
-     
+    width = image.shape[1]     
     poly = np.array([[(50, height - 30), (width - 50, height  - 30), (width / 2 + 0, height / 2 + 25 ), (width / 2 - 200, height / 2 + 25)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, np.int32(poly), 255)
@@ -30,12 +30,15 @@ def displayLines(image, lines):
     if lines is not None:
         for line in lines:
            x1, y1, x2, y2 = line.reshape(4)
+           print(myJSON.lineToJSON(x1, y1, x2, y2))
            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
     return lineImage
 
 source = "video2.mp4"
 cap = cv2.VideoCapture(source)
 #cap = cv2.VideoCapture(1)
+
+
 while(True):
     ret, frame = cap.read()
     #solo in caso di video, al termine viene rivisualizzato
@@ -48,8 +51,9 @@ while(True):
     roi = regionOfInterest(canny_Image)
 
     lines = cv2.HoughLinesP(roi, 2, np.pi / 180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
-
+    
     linesImage =  displayLines(roi, lines)
+    #print(linesImage)
 
     cv2.imshow('frame', frame)
     #cv2.imshow('canny', canny_Image)
@@ -60,3 +64,4 @@ while(True):
 
 cap.release()
 cv2.destroyAllWindows() 
+
