5437ea14c35fd16b92525445453e6fbd77312a8e
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2022-02-25 15:52:41 +0100

    box per le segnalazioni, librerie offline

--- EEL-application/web/scripts/animations.js
+++ EEL-application/web/scripts/animations.js
@@ -1,11 +1,40 @@
-$(document).ready(function(){
+var alertsDisplayed = false;
+
+$(document).ready(function () {
     $("#navigateframe").hide();
+    $(".alertslist").hide();
+    alertsDisplayed = false;
+
+    $(".close").click(function(){
+        hideAlerts();
+    });
+
+    $(".carinfo").click(function(){
+        hideAlerts();
+    });
+
+
 });
 
-function hide(){
+
+function hideNavigation() {
     $("#navigateframe").hide(250, "linear");
 }
 
-function show(){
+function showNavigation() {
     $("#navigateframe").show(250, "linear");
+}
+
+function hideAlerts() {
+    if (alertsDisplayed) {
+        $(".alertslist").hide(250, "linear");
+        alertsDisplayed = false;
+    }
+}
+
+function showAlerts() {
+    if (!alertsDisplayed) {
+        $(".alertslist").show(250, "linear");
+        alertsDisplayed = true;
+    }
 }
