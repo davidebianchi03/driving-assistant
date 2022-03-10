89c23bc57a7cc4b0f393afcdfafeaab452e9ed2d
Emanuele <82151522+LelePaci@users.noreply.github.com>
2022-03-10 13:30:40 +0100

    parsing dati sensori in json

--- Arduino/rilevamento-distanza/rilevamento-distanza.ino
+++ Arduino/rilevamento-distanza/rilevamento-distanza.ino
@@ -1,5 +1,6 @@
-//F -> FRONT | B -> BACK
-//L -> SINISTRA | R -> DESTRA | M -> CENTRO
+#include <ArduinoJson.h>
+
+
 #define pinEchoBL 2
 #define pinTriggerBL 3
 #define pinEchoBM 4
@@ -14,6 +15,19 @@
 #define pinTriggerFL 13
 
 #define S_NUM 6
+
+
+/*
+  F -> FRONT | B -> BACK
+  L -> SINISTRA | R -> DESTRA | M -> CENTRO
+  ORDINE SENSORI
+  back-left
+  back-middle
+  back-right
+  front-right
+  front-middle
+  front-left
+*/
 
 int pinsTrigger[] = {3, 5, 7, 9, 11, 13};
 int pinsEcho[] = {2, 4, 6, 8, 10, 12};
@@ -30,14 +44,24 @@ void setup()
 
 
 void loop()
-{
-  for (int i = 0; i < S_NUM; i++) {
-    double distance = calcDistance(pinsTrigger[i], pinsEcho[i]);
-    Serial.print(String(i) + " --");
-    Serial.println(distance);
+{ /*
+    double[] distances = {}
+    for (int i = 0; i < S_NUM; i++) {
+     double distance = calcDistance(pinsTrigger[i], pinsEcho[i]);
+     Serial.print(String(i) + " --");
+     //Serial.println(distance);
+     Serial.println(toJSON(
 
+    }
+    calcDistance(pinTriggerFL, pinEchoFL);*/
+  double distances[6];
+
+  for (int i = 0; i < 6; i++) {
+    distances[i] = calcDistance(pinsTrigger[i], pinsEcho[i]);
+    delay(10);
   }
-  calcDistance(pinTriggerFL, pinEchoFL);
+  String jsonString = toJSON(distances[0], distances[1], distances[2], distances[3], distances[4], distances[5]);
+
 }
 
 double calcDistance(int pinTrigger, int pinEcho) {
@@ -52,11 +76,16 @@ double calcDistance(int pinTrigger, int pinEcho) {
 }
 
 
-class Rilevazione {
-  public:
-    String sensorName;
-    double sensorDistance;
-    Rilevazione(String name, double distance);
+String toJSON(double bl, double bm, double, br, double fr, double fm, double fl) {
+  StaticJsonDocument<200> doc;
+  doc["fl"] = fl;
+  doc["fm"] = fm;
+  doc["fr"] = fr;
+  doc["bl"] = bl;
+  doc["bm"] = bm;
+  doc["br"] = br;
 
-  private:
+  String jsonData;
+  serializeJson(doc, jsonData);
+  return jsonData;
 }
