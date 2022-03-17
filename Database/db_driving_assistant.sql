23bf17d253f2e8aec134df43b4e077662cd83bdb
Emanuele <82151522+LelePaci@users.noreply.github.com>
2022-03-17 12:53:37 +0100

    Create db_driving_assistant.sql

---
+++ Database/db_driving_assistant.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mar 17, 2022 alle 12:52
+-- Versione del server: 10.4.6-MariaDB
+-- Versione PHP: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_driving_assistant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `segnalazioni`
+--
+
+CREATE TABLE `segnalazioni` (
+  `ID` int(11) NOT NULL,
+  `UserIdentifier` int(11) NOT NULL,
+  `Titolo` varchar(100) NOT NULL,
+  `Descrizione` text DEFAULT NULL,
+  `Latitudine` double NOT NULL,
+  `Longitudine` double NOT NULL,
+  `Accettato` bit(1) NOT NULL DEFAULT b'0',
+  `Completato` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `Nome` varchar(255) NOT NULL,
+  `Cognome` varchar(255) NOT NULL,
+  `DataAcquisto` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `veicoli`
+--
+
+CREATE TABLE `veicoli` (
+  `IDVeicolo` int(11) NOT NULL,
+  `Marca` varchar(255) NOT NULL,
+  `Modello` varchar(255) NOT NULL,
+  `Targa` varchar(7) NOT NULL,
+  `IDProprietario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `segnalazioni`
+--
+ALTER TABLE `segnalazioni`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `UserIdentifier` (`UserIdentifier`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indici per le tabelle `veicoli`
+--
+ALTER TABLE `veicoli`
+  ADD PRIMARY KEY (`IDVeicolo`,`Targa`),
+  ADD KEY `IDProprietario` (`IDProprietario`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `segnalazioni`
+--
+ALTER TABLE `segnalazioni`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `veicoli`
+--
+ALTER TABLE `veicoli`
+  MODIFY `IDVeicolo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `segnalazioni`
+--
+ALTER TABLE `segnalazioni`
+  ADD CONSTRAINT `segnalazioni_ibfk_1` FOREIGN KEY (`UserIdentifier`) REFERENCES `users` (`UserID`);
+
+--
+-- Limiti per la tabella `veicoli`
+--
+ALTER TABLE `veicoli`
+  ADD CONSTRAINT `veicoli_ibfk_1` FOREIGN KEY (`IDProprietario`) REFERENCES `users` (`UserID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
