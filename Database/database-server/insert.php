a150bab49de4ab2f3d0c5398c9ddf6ba3ac51567
Emanuele <paciemanuele.2@gmail.com>
2022-03-18 17:07:33 +0100

    aggiunto switch per selezione insert

--- Database/database-server/insert.php
+++ Database/database-server/insert.php
@@ -5,35 +5,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
 
-    $userID =  $title =  $description = $lat = $lon = "";
+    require_once 'config.php';
+    if (isset($data["action"])) {
+        switch ($data["action"]) {
+            case "insert-segnalazione":
+                $userID =  $title =  $description = $lat = $lon = "";
+                if (isset($data['user_id']) && isset($data['title']) && isset($data['description']) && isset($data['lat']) && isset($data['lon'])) {
+                    $userID = trim($data['user_id']);
+                    $title = trim($data['title']);
+                    $description = trim($data['description']);
+                    $lat = trim($data['lat']);
+                    $lon = trim($data['lon']);
 
-    if (isset($data['user_id']) && isset($data['title']) && isset($data['description']) && isset($data['lat']) && isset($data['lon'])) {
-        $userID = trim($data['user_id']);
-        $title = trim($data['title']);
-        $description = trim($data['description']);
-        $lat = trim($data['lat']);
-        $lon = trim($data['lon']);
 
-        require_once 'config.php';
-        $sql = "INSERT INTO segnalazioni (UserIdentifier, Titolo, Descrizione, Latitudine, Longitudine) VALUES (?, ?, ?, ?, ?)";
+                    $sql = "INSERT INTO segnalazioni (UserIdentifier, Titolo, Descrizione, Latitudine, Longitudine) VALUES (?, ?, ?, ?, ?)";
 
-        if ($stmt = mysqli_prepare($link, $sql)) {
-            mysqli_stmt_bind_param($stmt, "sssss", $param_userID, $param_title, $param_description, $param_lat, $param_lon);
+                    if ($stmt = mysqli_prepare($link, $sql)) {
+                        mysqli_stmt_bind_param($stmt, "sssss", $param_userID, $param_title, $param_description, $param_lat, $param_lon);
 
-            $param_userID = $userID;
-            $param_title = $title;
-            $param_description = $description;
-            $param_lat = $lat;
-            $param_lon = $lon;
+                        $param_userID = $userID;
+                        $param_title = $title;
+                        $param_description = $description;
+                        $param_lat = $lat;
+                        $param_lon = $lon;
 
-            if (mysqli_stmt_execute($stmt)) {
-                exit();
-            } else {
-                InternalServerError_500();
-            }
-            mysqli_stmt_close($stmt);
+                        if (mysqli_stmt_execute($stmt)) {
+                            exit();
+                        } else {
+                            InternalServerError_500();
+                        }
+                        mysqli_stmt_close($stmt);
+                    }
+                    mysqli_close($link);
+                } else {
+                    BadRequest_400();
+                }
+                break;
+            case "insert-user":
+
+                break;
         }
-        mysqli_close($link);
     } else {
         BadRequest_400();
     }
