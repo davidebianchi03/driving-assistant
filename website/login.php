4cfc7a3197c6d4e38da201517534a86cd47604d8
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2022-03-21 18:06:28 +0100

    Gestione del login,
    logout,
    pagina per controllo se la email è valida

--- website/login.php
+++ website/login.php
@@ -16,7 +16,7 @@
         <div class="blur">
             <div class="form-container">
                 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="login-form">
-                    <p class = "title">Accedi alla mappa delle segnalazioni</p>
+                    <p class="title">Accedi alla mappa delle segnalazioni</p>
                     <p class="input">
                         <label for="username">Username</label>
                         <input type="text" name="username">
@@ -26,7 +26,46 @@
                         <input type="password" name="password">
                     </p>
                     <input type="submit" value="Login" class="submit-btn">
-                    <p><a href="register.php" class = "register">Registrati</a></p>
+                    <?php
+                    require_once 'DBconfig.php';
+                    session_start();
+                    if (isset($_SESSION['session_id']) && !empty($_SESSION['session_id'])) {
+                        header("location:map.php");
+                        exit();
+                    }
+
+                    if (
+                        isset($_POST['username']) && !empty(trim($_POST['username'])) &&
+                        isset($_POST['password']) && !empty(trim($_POST['password']))
+                    ) {
+                        $sql = "SELECT * FROM users WHERE Username = ?";
+                        if ($stmt = mysqli_prepare($link, $sql)) {
+                            mysqli_stmt_bind_param($stmt, 's', $_POST['username']);
+                            if (mysqli_stmt_execute($stmt)) {
+                                $result = mysqli_stmt_get_result($stmt);
+                                if (mysqli_num_rows($result) == 1) {
+                                    $row = mysqli_fetch_array($result);
+                                    //controllo la password
+                                    if (password_verify($_POST['password'], $row['Password'])) {
+                                        $_SESSION['session_id'] = $row['UserID'];
+                                        header("location:map.php");
+                                        exit();
+                                    } else {
+                                        echo '<p class = "error">Username o password errati</p>';
+                                    }
+                                } else {
+                                    echo '<p class = "error">Username o password errati</p>';
+                                }
+                            } else {
+                                echo "Errore durante l'esecuzione della query";
+                            }
+                            mysqli_stmt_close($stmt);
+                        } else {
+                            echo "Errore nella preparazione dello statement";
+                        }
+                    }
+                    ?>
+                    <p><a href="register.php" class="register">Registrati</a></p>
                 </form>
             </div>
         </div>
--- website/map.php
+++ website/map.php
@@ -1,17 +1,5 @@
 <?php
 require_once 'DBconfig.php';
-if (isset($_GET['userid']) && !empty(trim($_GET['userid']))) {
-    $sql = 'UPDATE users SET VerifyEmail = 1 WHERE UserID = ?';
-    if ($stmt = mysqli_prepare($link, $sql)) {
-        mysqli_stmt_prepare($stmt, 'i', $_GET['userid']);
-        if (!mysqli_stmt_execute($stmt)) {
-            echo "Errore nell'esecuzione della query";
-        }
-    } else {
-        echo 'Errore nella preparazione dello statement';
-    }
-    mysqli_stmt_close($stmt);
-}
 //autenticazione dell'utente
 session_start();
 //controllo il login
@@ -25,16 +13,14 @@ if (isset($_SESSION['session_id']) && !empty(trim($_SESSION['session_id']))) {
             if (mysqli_num_rows($result) == 1) {
                 //utente autenticato
                 $row = mysqli_fetch_array($result);
-                if($row['VerifyEmail'] == 0){
+                if ($row['VerifyEmail'] == 0) {
                     //mail non verificata
-                    // header('location:waitmailconfirm.php');
+                    header('location:waitmailconfirm.php');
                     exit();
-                }
-                else{
+                } else {
                     header('location:map.php');
                     exit();
                 }
-                
             } else {
                 echo "Utente non autenticato";
                 session_destroy();
@@ -59,24 +45,4 @@ if (isset($_SESSION['session_id']) && !empty(trim($_SESSION['session_id']))) {
     exit();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Driving assistant</title>
-    <link rel="shortcut icon" href="img/icon.png">
-    <link rel="stylesheet" href="styles/reservedarea.css">
-</head>
-
-<body>
-    <div class = "nav">
-        <a href="logout.php">Logout</a>
-    </div>
-    vxcvxv
-</body>
-
-</html>
+?>
