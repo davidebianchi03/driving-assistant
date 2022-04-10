1cd44268a0991fcf329c9e3b854e69164f38893c
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2022-04-10 09:26:19 +0200

    Sistemata invio risposta alla richiesta di invio email, requirements.txt

--- email-sender-server/emailsender/views.py
+++ email-sender-server/emailsender/views.py
@@ -7,9 +7,24 @@ token = ""
 
 @csrf_exempt
 def sendEmailView(request):
+    response_dict = dict()
     if request.method == "POST":
         request_body = json.loads(request.body.decode('UTF-8'))
         if 'token' in request_body.keys() and 'subject' in request_body.keys() and 'body' in request_body.keys() and 'reciver' in request_body.keys():
             if(request_body['token'] == token):
-                sendEmail.sendEmail(receiver_email=request_body['reciver'],subject=request_body['subject'],body=request_body['body'])
-    return HttpResponse(request, 'ok')
+                result = sendEmail.sendEmail(receiver_email=request_body['reciver'],subject=request_body['subject'],body=request_body['body'])
+                if result:
+                    response_dict["result"] = "ok"
+                else:
+                    response_dict["result"] = "error"
+                    response_dict["message"] = "error in sending the email"
+            else:
+                response_dict["result"] = "error"
+                response_dict["message"] = "not authenticated"
+        else:
+            response_dict["result"] = "error"
+            response_dict["message"] = 'all fields were not copleted correctly'
+    else:
+        response_dict["result"] = "error"
+        response_dict["message"] = 'bad request method'
+    return HttpResponse(json.dumps(response_dict))
